b574322439a8d9fa87745a390d02d4dbde1a8e2b
octofire <nathanbrown8910@gmail.com>
2019-08-28 13:43:11 -0700

    Added Player template, projectile and utilities script

--- src/player.py
+++ src/player.py
@@ -1,1 +1,41 @@
 import pygame
+import toolbox
+import projectile
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, screen, x, y):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("../assets/Player_01.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.x, self.y)
+        self.speed = 5
+        self.angle = 0
+        self.shoot_cooldown = 0
+        self.shoot_cooldown_max = 10
+
+
+    def update(self):
+        self.rect.center = (self.x, self.y)
+
+        if self.shoot_cooldown > 0:
+            self.shoot_cooldown -= 1
+
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        self.angle = toolbox.angleBetweenPoints(self.x, self.y, mouse_x, mouse_y)
+
+        image_to_draw, image_rect = toolbox.getRotatedImage(self.image, self.rect, self.angle)
+
+
+        self.screen.blit(image_to_draw, image_rect)
+
+    def move (self, x_movement, y_movement):
+        self.x += self.speed * x_movement
+        self.y += self.speed * y_movement
+
+    def shoot(self):
+        if self.shoot_cooldown <= 0:
+            self.shoot_cooldown = self.shoot_cooldown_max
+            projectile.Bullet(self.screen, self.x, self.y, self.angle)
---
+++ src/projectile.py
@@ -0,0 +1,39 @@
+import pygame
+import toolbox
+import math
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, screen, x, y, angle):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.angle = angle
+        self.image = pygame.image.load("../assets/Balloon.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.x, self.y)
+        self.image, self.rect = toolbox.getRotatedImage(self.image, self.rect, self.angle)
+        self.speed = 10
+        self.angle_rads = math.radians(self.angle)
+        self.x_move = math.cos(self.angle_rads) * self.speed
+        self.y_move = -math.sin(self.angle_rads) * self.speed
+        self.damage = 5
+        
+    def update(self):
+        
+        self.rect.center = (self.x, self.y)
+
+        if self.x < -self.image.get_width():
+            self.kill()
+        elif self.x > self.screen.get_width() + self.image.get_width():
+            self.kill()
+        elif self.y < -self.image.get_height():
+            self.kill()
+        elif self.y > self.screen.get_height() + self.image.get_height():
+            self.kill()
+        
+
+        self.screen.blit(self.image, self.rect)
+
+    def explode(self):
+        self.kill()
