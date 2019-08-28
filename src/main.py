28d7af90f357e3e8884910633e1fc01055669b1a
octofire <nathanbrown8910@gmail.com>
2019-08-28 13:48:31 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pygame
-
-
+from Player import Player
+from projectile import Bullet
+from enemy import Enemy
 # Start the game
 pygame.init()
 game_width = 1000
@@ -8,6 +9,21 @@ game_height = 650
 screen = pygame.display.set_mode((game_width, game_height))
 clock = pygame.time.Clock()
 running = True
+
+background_image = pygame.image.load("../")
+
+playerGroup = pygame.sprite.Group()
+projectilesGroup = pygame.sprite.Group()
+enemiesGroup = pygame.sprite.Group()
+
+Player.containers = playerGroup
+Bullet.containers = projectilesGroup
+Enemy.containers = enemiesGroup
+
+mr_player = Player(screen, game_width/2, game_height/2)
+
+Enemy (screen, 100, 100, mr_player)
+Enemy (screen, 100, 500, mr_player)
 
 
 # ***************** Loop Land Below *****************
@@ -19,6 +35,28 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             running = False
+
+             keys = pygame.key.get_pressed()
+    if keys[pygame.K_d]:
+        mr_player.move(1, 0)
+    if keys[pygame.K_a]:
+        mr_player.move(-1, 0)
+    if keys[pygame.K_w]:
+        mr_player.move(0, -1)
+    if keys[pygame.K_s]:
+        mr_player.move(0, 1)
+    if pygame.mouse.get_pressed()[0]:
+        mr_player.shoot()
+
+
+    screen.blit(background_image, (0, 0))
+
+    mr_player.update()
+
+    for projectile in projectilesGroup:
+        projectile.update()
+    for enemy in enemiesGroup:
+        enemy.update(projectilesGroup)
 
     # Tell pygame to update the screen
     pygame.display.flip()
